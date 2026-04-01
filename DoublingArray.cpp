0401bb948348610a3d7ad853ebe0778c98115ab1
DeceptiKHAAAAN <127812076+DeceptiKHAAAAN@users.noreply.github.com>
2026-04-01 10:36:28 -0500

    Added Comments and a return statement for the main function

--- DoublingArray.cpp
+++ DoublingArray.cpp
@@ -5,9 +5,11 @@ int* DoubleArray(const int arr[], int size);
 
 int main()
 {
+    // Initialize Variables
     int arr[] = {1, 2, 3, 4, 5};
     int size = 5;
 
+    // Prints the array before it doubled
     cout << "The array before doubling: { ";
     for (int i = 0; i < size - 1; i++)
     {
@@ -15,21 +17,27 @@ int main()
     }
     cout << arr[size - 1] << " }\n\n";
 
+    // Doubles the array through the DoubleArray function
     int* newArr = DoubleArray(arr, size);
     size *= 2;
     
+    // Prints the array after doubling
     cout << "the array after doubling: { ";
     for (int i = 0; i < size - 1; i++)
     {
         cout << newArr[i] << " , ";
     }
     cout << newArr[size - 1] << " }";
+
+    return 0;
 }
 
 int* DoubleArray(const int arr[], int size)
 {
+    // Creates a pointer to a new array
     int* newArr = new int[size * 2];
 
+    // Copies the contents of the old array to the new array that is twice in size
     for (int i = 0; i < size * 2; i++)
     {
         if(i < size)
